65cf5d504bf6ae49acf01a196fc8a484f1de9e69
Jesse Shawl <jesse@jshawl.com>
2015-05-04 10:57:47 -0400

    complete fourth view controller

--- Lesson07/FourthViewController.swift
+++ Lesson07/FourthViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class FourthViewController: UIViewController {
 
+    @IBOutlet weak var textView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let documentsPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as NSString
+        var fullPath = documentsPath.stringByAppendingPathComponent("three.txt")
+        let txt:String = String(contentsOfFile: fullPath, encoding: NSUTF8StringEncoding, error: nil)!
+        textView.text = txt
+        
     }
 }
